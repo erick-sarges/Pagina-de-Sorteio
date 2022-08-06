3cbf4b2ee1ec1a5c94853f8ea6585bd19433a8a1
erick <erick.sarges0223@gmail.com>
2022-08-06 14:22:16 -0300

    projeto finalizado

--- script.js
+++ script.js
@@ -1,20 +1,37 @@
+
 let h1 = document.getElementsByClassName("cabeca")[0];
 
 h1.addEventListener("mousemove", mudarCor);
 h1.addEventListener("mouseout", voltarCor);
 
-function mudarCor(e){
+function mudarCor(){
 
  
- this.style.color = "lime";
+ this.style.color = "#ff1493";
+ 
 
 }
 
- function voltarCor(e){
+ function voltarCor(){
 
-    this.style.color = "#ff1493";
+    this.style.color = "#00ff00";
+    
 
  };
 
+let botao = document.getElementById("botao");
+botao.addEventListener("click", sortear)
 
+
+let pessoas = ["Erick", "Maria", "Genoveva", "Pierre", "Rudolf"];
+
+function sortear(){
+
+    let rifas = pessoas.length;
+    
+    let numeroDaSorte = Math.floor(Math.random() * rifas);
+
+    document.getElementById("h2").innerHTML = pessoas[numeroDaSorte];
+
+};
 
